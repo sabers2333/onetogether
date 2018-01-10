b3343362220cdf3a2cef88a0e69a5188b7acc6c3
haiqing.lin <1047534821@qq.com>
2018-01-10 17:17:23 +0800

    1

--- tpl/Index/Index/detail.php
+++ tpl/Index/Index/detail.php
@@ -12,7 +12,7 @@
                             <h1 class="page-title"><span>{$info.name}</span></h1>
                         </div>
                     </div>
-                    <div class="wpb_row vc_row-fluid row_width1000 have_left_side" style="margin-bottom: 25px;">
+                    <div class="wpb_row vc_row-fluid row_width960 have_left_side" style="margin-bottom: 25px;">
                         <div class="vc_span9 wpb_column column_container">
                             <div class="wpb_wrapper">
                                 <header class="entry-header entry-meta"><span class="author"><i class="icon-pencil"></i>By <a href="/index.html" rel="author">一起手游吧</a></span><span class="date"><i class="icon-calendar-empty"></i><a ><time >{$info.last_time|date='Y-m-d H:i:s',###}</time></a></span><span class="category"><i class="icon-folder-open"></i><a href="/news.html">新闻动态</a></span>阅读 {$info.click} 次</header><!-- .entry-header -->
