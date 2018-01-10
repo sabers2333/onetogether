6c813f361858f6596e0edf89a78c060267836879
haiqing.lin <1047534821@qq.com>
2018-01-10 18:22:41 +0800

    1

--- tpl/Index/Public/footer.php
+++ tpl/Index/Public/footer.php
@@ -3,25 +3,30 @@
         <div id="sub-footer-core">
             <div id="footer-menu" class="sub-footer-links"><ul id="menu-footermenuall" class="menu">
                 <li id="menu-item-279" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-279">
-                    <a href="pinpai.html">酒窝品牌</a>
+                    <a href="pinpai.html">品牌文化</a>
                     <ul class="sub-menu">
                         <li id="menu-item-280" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-280"><a href="pinpai.html">简介</a></li>
                     </ul>
                 </li>
                 <li id="menu-item-278" class="menu-item menu-item-type-taxonomy menu-item-object-category current-category-ancestor current-menu-ancestor current-menu-parent current-category-parent menu-item-has-children menu-item-278">
-                    <a href="food.html">一起手游吧</a>
+                    <a href="food.html">美食</a>
                     <ul class="sub-menu">
-                        <li  class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-778"><a href="food.html?tid=1"  title="经典港式">经典港式</a></li>
+                        <li  class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-778"><a href="food.html?cat_id=24"  title="西餐">西餐</a></li>
+                        <li  class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-778"><a href="food.html?cat_id=25"  title="中餐">中餐</a></li>
+                        <li  class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-778"><a href="food.html?cat_id=26"  title="饮品">饮品</a></li>
+                        <li  class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-778"><a href="food.html?cat_id=27"  title="烧烤">烧烤</a></li>
+                        <li  class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-778"><a href="food.html?cat_id=28"  title="特色">特色</a></li>
                     </ul>
                 </li>
                 <li id="menu-item-317" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-317">
-                    <a href="lianxi.html">全球网点</a>
+                    <a href="news.html?cat_id=5">新闻动态</a>
                     <ul class="sub-menu">
-                        <li id="menu-item-781" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-781"><a href="dianPu.html">店铺详情</a></li>
+                        <li id="menu-item-781" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-781"><a href="news.html?cat_id=5">最新新闻</a></li>
+                        <li id="menu-item-781" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-781"><a href="news.html?cat_id=7">最新产品</a></li>
                     </ul>
                 </li>
                 <li id="menu-item-782" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-has-children menu-item-782">
-                    <a href="zhaoshang.html">加盟商专区</a>
+                    <a href="lianxi.html">联系我们</a>
                     <ul class="sub-menu">
                         <li id="menu-item-2234" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-2234"><a href="zhaoshang.html">招商加盟</a></li>
                     </ul>
