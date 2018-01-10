0bf320484f2061e8161cd2a19ea33fe0663d0faa
haiqing.lin <1047534821@qq.com>
2018-01-10 17:28:37 +0800

    1

--- tpl/Index/Index/detail.php
+++ tpl/Index/Index/detail.php
@@ -13,11 +13,11 @@
                         </div>
                     </div>
                     <div class="wpb_row vc_row-fluid row_width961 have_left_side" style="margin-bottom: 25px;">
-                        <div class="vc_span9 wpb_column column_container">
+                        <div class="vc_span91 wpb_column column_container">
                             <div class="wpb_wrapper">
                                 <header class="entry-header entry-meta"><span class="author"><i class="icon-pencil"></i>By <a href="/index.html" rel="author">一起手游吧</a></span><span class="date"><i class="icon-calendar-empty"></i><a ><time >{$info.last_time|date='Y-m-d H:i:s',###}</time></a></span><span class="category"><i class="icon-folder-open"></i><a href="/news.html">新闻动态</a></span>阅读 {$info.click} 次</header><!-- .entry-header -->
                                
-                                <div class="entry-content" style="width:680px;line-height: 24px;">{$info.content}</div>
+                                <div class="entry-content" style="line-height: 24px;">{$info.content}</div>
 
                                 <!-- .entry-content --><nav role="navigation" id="nav-below">
                                 <div class="nav-previous">
