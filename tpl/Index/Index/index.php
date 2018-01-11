216c3d4cc304200ce0941e4e9971387877bba6ff
haiqing.lin <1047534821@qq.com>
2018-01-11 16:56:45 +0800

    1

--- tpl/Index/Index/index.php
+++ tpl/Index/Index/index.php
@@ -53,7 +53,7 @@
 </div>
 <div class="food">
      <div class="title">
-         <h3>一起休闲餐厅</h3>
+         <h3>吃货&nbsp;&nbsp;【美食】</h3>
          <p>品味世界美食&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;给您味蕾绽放的快感</p>
      </div>
      <div class="w16">
