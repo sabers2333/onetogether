a6ad82d220a2257d64938273b520f6fa412084e4
project.php <1047534821@qq.com>
2018-04-03 00:24:08 +0800

    1

--- tpl/Index/Index/wap.php
+++ tpl/Index/Index/wap.php
@@ -17,151 +17,44 @@
 </style>
 <script type='text/javascript' src='{$static_path}js/jquery.js?ver=1.11.1'></script>
 <body>
-
-	<div class="img">
-		
-			<img src="{$static_path}/zswap/4_01.png"   alt="">
-	
-	
-		
-			<img src="{$static_path}/zswap/4_02.png"   alt="">
-	
-	
-		
-			<img src="{$static_path}/zswap/4_03.png"   alt="">
-	
-	
-		
-			<img src="{$static_path}/zswap/4_04.png"   alt="">
-	
-	
-		
-			<img src="{$static_path}/zswap/4_05.png"   alt="">
-	
-	
-		
-			<img src="{$static_path}/zswap/4_06.png"   alt="">
-	
-	
-		
-			<img src="{$static_path}/zswap/4_07.png"   alt="">
-	
-	
-		
-			<img src="{$static_path}/zswap/5_02.png"   alt="">
-			<img src="{$static_path}/zswap/5_03.png"   alt="">
-			<img src="{$static_path}/zswap/5_04.png"   alt="">
-			<img src="{$static_path}/zswap/5_06.png"   alt="">
-	
-			<div class="dt"><div class="dt1"><img src="{$static_path}images/1.gif" alt=""></div></div>
-	
-		
-			<img src="{$static_path}/zswap/4_11.png"   alt="">
-	
-	
-		
-			<img src="{$static_path}/zswap/4_12.png"   alt="">
-	
-	
-		
-			<img src="{$static_path}/zswap/4_13.png"   alt="">
-	
-	
-		
-			<img src="{$static_path}/zswap/4_14.png"   alt="">
-	
-	
-		
-			<img src="{$static_path}/zswap/4_15.png"   alt="">
-	
-	
-		
-			<img src="{$static_path}/zswap/4_16.png"   alt="">
-	
-	
-		
-			<img src="{$static_path}/zswap/4_17.png"   alt="">
-	
-	
-		
-			<img src="{$static_path}/zswap/4_18.png"   alt="">
-	
-	
-		
-			<img src="{$static_path}/zswap/4_19.png"   alt="">
-	
-	
-		
-			<img src="{$static_path}/zswap/4_20.png"   alt="">
-	
-	
-		
-			<img src="{$static_path}/zswap/4_21.png"   alt="">
-	
-	
-		
-			<img src="{$static_path}/zswap/4_22.png"   alt="">
-	
+<div class="img">
+	<img src="{$static_path}/wap/1_01.png"   alt="">
+	<img src="{$static_path}/wap/1_02.png"   alt="">
+	<img src="{$static_path}/wap/1_03.png"   alt="">
+	<img src="{$static_path}/wap/1_04.png"   alt="">
+	<img src="{$static_path}/wap/1_05.png"   alt="">
+	<img src="{$static_path}/wap/1_06.png"   alt="">
+	<img src="{$static_path}/wap/1_07.png"   alt="">
+	<img src="{$static_path}/wap/1_08.png"   alt="">
+	<img src="{$static_path}/wap/1_09.png"   alt="">
+	<img src="{$static_path}/wap/1_10.png"   alt="">
 	<div class="dt"><div class="dt1"><img src="{$static_path}images/1.gif" alt=""></div></div>
-		
-			<img src="{$static_path}/zswap/4_23.png"   alt="">
-	
-	
-		
-			<img src="{$static_path}/zswap/4_24.png"   alt="">
-	
-	
-		
-			<img src="{$static_path}/zswap/4_25.png"   alt="">
-	
-	
-		
-			<img src="{$static_path}/zswap/4_26.png"   alt="">
-	
-	
-		
-			<img src="{$static_path}/zswap/4_27.png"   alt="">
-	
-	
-		
-			<img src="{$static_path}/zswap/4_28.png"   alt="">
-	
-	
-		
-			<img src="{$static_path}/zswap/4_29.png"   alt="">
-	
-	
-		
-			<img src="{$static_path}/zswap/4_30.png"   alt="">
-	
-	
-		
-			<img src="{$static_path}/zswap/4_31.png"   alt="">
-	
-	
-		
-			<img src="{$static_path}/zswap/4_32.png"   alt="">
-	
-	
-		
-			<img src="{$static_path}/zswap/4_33.png"   alt="">
-	
-	
-		
-			<img src="{$static_path}/zswap/4_34.png"   alt="">
-	
-	
-		
-			<img src="{$static_path}/zswap/4_35.png"   alt="">
-	
-	
-		
-			<img src="{$static_path}/zswap/4_36.png"   alt="">
-	
-	
-		
-			<img src="{$static_path}/zswap/4_37.png"   alt="">
-			<div class="dt"><div class="dt1" style="margin-bottom: 200px;"><img src="{$static_path}images/1.gif" alt=""></div></div>
+	<img src="{$static_path}/wap/1_12.png"   alt="">
+	<img src="{$static_path}/wap/1_13.png"   alt="">
+	<img src="{$static_path}/wap/1_14.png"   alt="">
+	<img src="{$static_path}/wap/1_15.png"   alt="">
+	<img src="{$static_path}/wap/1_16.png"   alt="">
+	<img src="{$static_path}/wap/1_17.png"   alt="">
+	<img src="{$static_path}/wap/1_18.png"   alt="">
+	<img src="{$static_path}/wap/1_19.png"   alt="">
+	<img src="{$static_path}/wap/1_20.png"   alt="">
+	<img src="{$static_path}/wap/1_21.png"   alt="">
+	<img src="{$static_path}/wap/1_22.png"   alt="">
+	<img src="{$static_path}/wap/1_23.png"   alt="">
+	<img src="{$static_path}/wap/1_24.png"   alt="">
+	<div class="dt"><div class="dt1" style="margin-bottom: 200px;"><img src="{$static_path}images/1.gif" alt=""></div></div>
+	<img src="{$static_path}/wap/1_26.png"   alt="">
+	<img src="{$static_path}/wap/1_27.png"   alt="">
+	<img src="{$static_path}/wap/1_28.png"   alt="">
+	<img src="{$static_path}/wap/1_29.png"   alt="">
+	<img src="{$static_path}/wap/1_30.png"   alt="">
+	<img src="{$static_path}/wap/1_31.png"   alt="">
+	<img src="{$static_path}/wap/1_32.png"   alt="">
+	<img src="{$static_path}/wap/1_33.png"   alt="">
+	<img src="{$static_path}/wap/1_34.png"   alt="">
+	<img src="{$static_path}/wap/1_35.png"   alt="">
+	<img src="{$static_path}/wap/1_36.png"   alt="">
+	<div class="dt"><div class="dt1" style="margin-bottom: 200px;"><img src="{$static_path}images/1.gif" alt=""></div></div>
 
 </div>
     <div style="display: none;"><script src="https://s19.cnzz.com/z_stat.php?id=1272888463&web_id=1272888463" language="JavaScript"></script></div>
