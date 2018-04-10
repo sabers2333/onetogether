ab3d7b33457552b8fcde2f2167c184b2ea3808f5
haiqing.lin <1047534821@qq.com>
2018-04-10 13:19:59 +0800

    1

--- cms/Lib/Action/Wap/BaseAction.class.php
+++ cms/Lib/Action/Wap/BaseAction.class.php
@@ -42,7 +42,7 @@ class BaseAction extends CommonAction
 
         $this->merchant_info['wxname'] = $merchant['name'];
         $this->assign('wxuser', $this->merchant_info);
-        $this->static_path = 'http://zcplan.oss-cn-shenzhen.aliyuncs.com/uploads/public' .  '/static/default/';
+        $this->static_path = 'http://www.86teen.cn/Files/article/ueditor/php' .  '/static/default/';
              
         $this->assign('static_path', $this->static_path);
              
