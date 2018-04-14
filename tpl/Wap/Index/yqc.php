ee347019e6e0c5089024a5b93f13d0583fbcd08a
Unknown <1047534821@qq.com>
2018-04-14 14:54:07 +0800

    修改

--- tpl/Wap/Index/yqc.php
+++ tpl/Wap/Index/yqc.php
@@ -91,7 +91,6 @@ ul, ol, li{list-style:none;}
 	<div class="dt"><div class="dt1" style="margin-bottom: 200px;"><img src="{$static_path}images/1.gif" alt=""></div></div>
 
 </div>
-    <div style="display: none;"><script src="https://s19.cnzz.com/z_stat.php?id=1272888463&web_id=1272888463" language="JavaScript"></script></div>
 
 <script>(function() {var _53code = document.createElement("script");_53code.src = "https://tb.53kf.com/code/code/10170576/1";var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(_53code, s);})();</script> 
 <script>
