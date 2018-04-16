16901545949db5f16d3c9c6ce46d22864a470763
haiqing.lin <1047534821@qq.com>
2018-04-16 13:39:33 +0800

    修改

--- tpl/Index/Index/yqc.php
+++ tpl/Index/Index/yqc.php
@@ -219,7 +219,6 @@
        </div>
     </div>
 </div>
-<script>(function() {var _53code = document.createElement("script");_53code.src = "https://tb.53kf.com/code/code/10170576/1";var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(_53code, s);})();</script> 
 <script type="text/javascript">
 
 $(function(){
