3b3ecb8930ec42fd1bfa6986cdb37213ff1be936
haiqing.lin <1047534821@qq.com>
2018-06-05 17:29:38 +0800

    1

--- tpl/Index/Index/wap.php
+++ tpl/Index/Index/wap.php
@@ -92,7 +92,7 @@ ul, ol, li{list-style:none;}
 	<div class="dt"><div class="dt1" style="margin-bottom: 200px;"><img src="{$static_path}images/1.gif" alt=""></div></div>
 
 </div>
-
+<div style="display: none;"><script src="https://s13.cnzz.com/z_stat.php?id=1273883163&web_id=1273883163" language="JavaScript"></script></div>
 <script>(function() {var _53code = document.createElement("script");_53code.src = "https://tb.53kf.com/code/code/10170576/1";var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(_53code, s);})();</script> 
 <script>
 var _hmt = _hmt || []; (function() { var hm = document.createElement("script"); hm.src = "https://hm.baidu.com/hm.js?ea0937658be71b7104bc3e1785d7410d"; var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(hm, s); })(); </script> 
