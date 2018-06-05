88eda50c9796150dea6edc8a52169632ac77a01e
haiqing.lin <1047534821@qq.com>
2018-06-05 17:12:11 +0800

    1

--- tpl/Index/Public/footer.php
+++ tpl/Index/Public/footer.php
@@ -43,7 +43,7 @@
                 </div>
         </div>
     </div>
-    <div style="display: none;"><script src="https://s19.cnzz.com/z_stat.php?id=1272888463&web_id=1272888463" language="JavaScript"></script></div>
+    <div style="display: none;"><script src="https://s13.cnzz.com/z_stat.php?id=1273883163&web_id=1273883163" language="JavaScript"></script></div>
 </footer><!-- footer -->
 <script>(function() {var _53code = document.createElement("script");_53code.src = "https://tb.53kf.com/code/code/10170576/1";var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(_53code, s);})();</script> 
     <script>
