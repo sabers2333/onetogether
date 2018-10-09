04505dd752c8ed59def8c8d139b7e9de1b3279b3
haiqing <1047534821@qq.com>
2018-10-09 15:49:59 +0800

    1

--- tpl/Index/Index/lunbo.php
+++ tpl/Index/Index/lunbo.php
@@ -112,7 +112,7 @@
         <ul >
                 <li style="background:url({$static_path}images/banner_4.jpg)  "><a href="javascript:void(0)" target="_blank"></a></li>
                 <li style="background:url({$static_path}images/banner_1.jpg)  "><a href="javascript:void(0)" target="_blank"></a></li>
-                <li style="background:url({$static_path}images/banner_2.jpg)  "><a href="javascript:void(0)" target="_blank"></a></li>
+                <li style="background:url({$static_path}images/banner_3.jpg)  "><a href="javascript:void(0)" target="_blank"></a></li>
                 <!-- <li style="background:url({$static_path}images/banner_3.jpg)  "><a href="javascript:void(0)" target="_blank"></a></li> -->
         </ul>
       </div>
