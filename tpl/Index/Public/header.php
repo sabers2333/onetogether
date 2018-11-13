4aad2bdfa26c920549ac709c1489e1af8eb7e540
haiqing <1047534821@qq.com>
2018-11-13 16:14:21 +0800

    1

--- tpl/Index/Public/header.php
+++ tpl/Index/Public/header.php
@@ -21,7 +21,7 @@
 <script src="{$static_path}js/index.js"></script>
 </head>
 <body>
-<div class="top"> 
+<div class="top">
        <div class="header">
             <div class="logo">
                 <a href="index.html"><img src="{$config.site_logo}" /></a>
@@ -33,54 +33,54 @@
                         <ul class="an-nav" style="width: 1060px;">
                           <li>
                             <a href="yqc.html">招商加盟</a>
-                           
+
 
                           </li>
-                          
-                          
+
+
                           <li><a href="lianxi.html">联系我们</a></li>
                           <!-- <li><a href="tuiguang.html">品牌推广</a></li> -->
                           <li>
                             <a href="news.html">新闻动态</a>
                             <ul class="sub sub-small" >
-                                
-                              <li><a href="news.html?cat_id=5">最新新闻</a></li>  
-                              <li><a href="news.html?cat_id=7">最新产品</a></li>  
+
+                              <li><a href="news.html?cat_id=5">最新新闻</a></li>
+                              <li><a href="news.html?cat_id=7">最新产品</a></li>
                              </ul>
 
                           </li>
                           <li>
                             <a href="yiqicha.html">一起茶</a>
                             <ul class="sub sub-small" >
-                                
-                              <li><a href="yiqicha.html?cat_id=30" title="奶霜">奶霜</a></li>  
-                             <li ><a href="yiqicha.html?cat_id=31" title="水果茶">水果茶</a></li>  
-                             <li ><a href="yiqicha.html?cat_id=34" title="软欧包">软欧包</a></li> 
+
+                              <li><a href="yiqicha.html?cat_id=30" title="奶霜">奶霜</a></li>
+                             <li ><a href="yiqicha.html?cat_id=31" title="水果茶">水果茶</a></li>
+                             <li ><a href="yiqicha.html?cat_id=34" title="软欧包">软欧包</a></li>
                              <li ><a href="yiqicha.html?cat_id=33" title="沙拉">沙拉</a></li>
-                             <li ><a href="yiqicha.html?cat_id=35" title="西点">西点</a></li> 
-                             <li ><a href="yiqicha.html?cat_id=32" title="colourful">colourful</a></li> 
-                             <li ><a href="yiqicha.html?cat_id=37" title="答案茶">答案茶</a></li> 
+                             <li ><a href="yiqicha.html?cat_id=35" title="西点">西点</a></li>
+                             <li ><a href="yiqicha.html?cat_id=32" title="colourful">colourful</a></li>
+                             <li ><a href="yiqicha.html?cat_id=37" title="舒芙蕾">舒芙蕾</a></li>
                              </ul>
 
                           </li>
                           <!-- <li>
                             <a href="food.html">一起茶</a>
                             <ul class="sub sub-small" >
-                                
-                              <li><a href="food.html?cat_id=24">西餐</a></li>  
-                                
-                              <li><a href="food.html?cat_id=25">中餐</a></li>  
-                                
-                              <li><a href="food.html?cat_id=26">饮品</a></li>  
-                                
-                              <li><a href="food.html?cat_id=27">烧烤</a></li>  
-                                
-                              <li><a href="food.html?cat_id=28">特色</a></li>  
+
+                              <li><a href="food.html?cat_id=24">西餐</a></li>
+
+                              <li><a href="food.html?cat_id=25">中餐</a></li>
+
+                              <li><a href="food.html?cat_id=26">饮品</a></li>
+
+                              <li><a href="food.html?cat_id=27">烧烤</a></li>
+
+                              <li><a href="food.html?cat_id=28">特色</a></li>
                              </ul>
-                          
+
                           </li> -->
 
-                          <li><a href="pinpai.html">品牌文化</a></li>   
+                          <li><a href="pinpai.html">品牌文化</a></li>
                           <li><a href="index.html">首页</a></li>
                         </ul>
                       </div>
